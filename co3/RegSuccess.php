28f272eb1b9a436430e96fbf66693fbd2f2ca1e6
sindoori-b <75937669+sindoori-b@users.noreply.github.com>
2021-03-14 20:07:54 +0530

    Add files via upload

---
+++ co3/RegSuccess.php
@@ -0,0 +1,12 @@
+<!DOCTYPE HTML>
+<html>
+<title>Success.php</title>
+<body>
+
+<div style=:text-align:left:><h1>Registration Successful </h1></div>
+<br/>
+
+<div style=:text-align: left:><a href="https://tkmce.ac.in">Please log-in to continue</a></div>
+
+</body>
+</html>
